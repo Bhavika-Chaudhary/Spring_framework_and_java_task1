d25df8ae55acf6a0700e5117c67483493f15a6b1
Bhavika Chaudhary <bhavikavedvan02@gmail.com>
2025-12-13 18:19:38 +0100

    Commit

---
+++ src/main/java/edu/vistula/task1/controller/HelloController.java
@@ -0,0 +1,23 @@
+package edu.vistula.task1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+//@RestController
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String hello(){return "Hello Vistula, my first Spring controller!"; }
+    //http://localhost:8080
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")String name, Model model){
+        model.addAttribute("name", name);
+        return  "greeting";
+    }
+    //http://localhost:8080/greeting?name=Vistula
+}
